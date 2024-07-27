7b2806d4e416f1ad86f2ff9038adc535b2e2c7fd
Yanina Ayala <yaninacayala@gmail.com>
2024-07-27 19:09:30 -0300

    se creo la db + metodos de express en index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-import express from "express"
+import express from "express";
+import db from "./database/db.json";
+
+const PORT = 8080;
+const app = express();
+
+app.get("/api", (request, response) => {
+  response.status(200).json(db.description);
+});
+
+app.get("/api/students", (request, response) => {
+  response.status(200).json(db.students);
+});
+
+app.get("/api/students/id", (request, response) => {
+  response.status(200).json(db.students);
+});
+
+app.get("/api/students/id/grades", (request, response) => {
+  response.status(200).json(db.students);
+});
+
+app.get("/api/teachers", (request, response) => {
+  response.status(200).json(db.teachers);
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on port: ", PORT);
+});
