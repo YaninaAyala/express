aa3e59a1e40239d5ee2359502f912e3997788988
Yanina Ayala <yaninacayala@gmail.com>
2024-07-28 20:41:34 -0300

    se modificó la funcion fetchData, y se completaron post, delete y patch en index

--- src/client.ts
+++ src/client.ts
@@ -1,23 +1,31 @@
+import { json } from "express";
+
 type method = "POST" | "GET" | "PUT" | "PATCH" | "DELETE";
 
-async function fetchData(data: { url: string; method?: method; body: object }) {  //CON LA ASIGNACION LE DEFINO EL VALOR POR DEFECTO QUE TOMARÁ EL PARAMETRO CUANDO NO ME ENVIEN NADA AL INVOCAR LA FUNCION
-  const response = await fetch(`http:localhost:8080/${data.url}`, {
-    method: data.method, //SE PUEDE SIMPLIFICAR DEJANDO SOLO METHOD Y COMO VALOR SE COLOCARÁ LA VARIABLE CON ESE MISMO NOMBRE
-    body: JSON.stringify(data.body),
+async function fetchData(params: {url: string; method?: method; body?: object;
+}) {
+  //CON LA ASIGNACION LE DEFINO EL VALOR POR DEFECTO QUE TOMARÁ EL PARAMETRO CUANDO NO ME ENVIEN NADA AL INVOCAR LA FUNCION
+
+  const response = await fetch(`http://localhost:8080/${params.url}`, {
+    method: params.method, //SE PUEDE SIMPLIFICAR DEJANDO SOLO METHOD Y COMO VALOR SE COLOCARÁ LA VARIABLE CON ESE MISMO NOMBRE
+    body: JSON.stringify(params.body),
+    headers: { "content-type": "application/json" },
   });
 
   console.log(response.status);
 
-  return await response.json();
+  const data = await response.json();
+  console.log(data);
+
+  return data;
 }
 
 async function main() {
   const data = await fetchData({
-    url: "students",
-    body: { name: "Sofia", age: 22 },
-    method: "POST",
+    url: "api",
+    //body: { id: 1},
+    method: "GET",
   });
-  console.log(data);
 }
 
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,68 @@
-import express from "express";
+import express, { json, request, response } from "express";
 import db from "./database/db.json";
+import { writeFileSync } from "jsonfile";
 
 const PORT = 8080;
 const app = express();
 
-app.get("/api", (request, response) => {
+function validateId(request, response, next) {
+  if (!request.body.id)
+    return response.status(400).json({ message: "No se envió ID" });
+  next();
+}
+
+function addProperty(request, response, next) {
+  request.user = "Yani"
+  next();
+}
+
+app.use(json());
+app.use(addProperty);
+
+app.get("/api", (request: any, response) => {
+  console.log(request.user); //request["user"] es lo mismo
+  
   response.status(200).json(db.description);
 });
 
-app.get("/students", (request, response) => {
+app.get("/api/students", (request, response) => {
   response.status(200).json(db.students);
 });
 
-app.get("/teachers", (request, response) => {
+app.get("/api/teachers", (request, response) => {
   response.status(200).json(db.teachers);
 });
 
-app.post("/students", (request, response) => {
-  console.log(request.body);
-  response.status(201).json({message: "SE CREÓ EL RECURSO"});
+app.post("/api/students", (request, response) => {
+  const student = request.body;
+  db.students.push(student);
+  writeFileSync("./src/database/db.json", db);
+
+  response.status(201).json({ message: "SE CREÓ EL RECURSO" });
+});
+
+app.post("/api/teachers", (request, response) => {
+  const student = request.body;
+  db.teachers.push(student);
+  writeFileSync("./src/database/db.json", db);
+
+  response.status(201).json({ message: "SE CREÓ EL RECURSO" });
+});
+
+app.delete("/api/students/id", validateId, (request, response) => {
+  const id = request.body.id;
+  const students = db.students.filter((student) => id != student.id);
+  db.students = students;
+  writeFileSync("./src/database/db.json", db);
+  response.status(200).json({ message: "SE ELIMINÓ EL STUDENT" });
+});
+
+app.patch("/api/students/id", validateId, (request, response) => {
+  const id = request.body.id;
+  const student = db.students.find((student) => id == student.id);
+  student.name = request.body.name;
+  writeFileSync("./src/database/db.json", db);
+  response.status(200).json({ message: "SE MODIFICÓ EL STUDENT" });
 });
 
 app.listen(PORT, () => {
